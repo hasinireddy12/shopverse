fa8d0f3d00c9d085a69c22fe49e5e125b1994353
hasinireddy12 <gaddamhasini08@gmail.com>
2026-06-20 22:02:37 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://shopverse-bxp8.onrender.com"
+  baseURL: "https://shopverse-bxp8.onrender.com/api"
 });
 
 export default api;
